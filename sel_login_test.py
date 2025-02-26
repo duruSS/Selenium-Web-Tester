3316ef4cc7a8f7e8a12ff2c455e9cf6501b4cf51
BarisYildirim7 <baris.yildirim@engardens.com>
2025-02-26 14:24:36 +0300

    SSO login tests implemented

--- sel_login_test.py
+++ sel_login_test.py
@@ -1,10 +1,17 @@
 from test import Test
 
 if __name__ == "__main__":
-    tester = Test("http://127.0.0.1:8003")
+    tester = Test("https://127.0.0.1:5000")
     
+    # Regular login Tests
     tester.test_login("baris@gmail.com", "asdasdasd")
     
     tester.test_login("baris@gmail.com", "test123")
     
     tester.test_login("baris@gmail.com", "test1234")
+
+    tester.test_login("baris@gmail.com", "' 1==1--")
+
+    # This test would not work in a realistic scenario
+    # since SSO login procedure cannot be automized.
+    tester.test_sso_login()
--- test.py
+++ test.py
@@ -4,7 +4,10 @@ from selenium.common.exceptions import NoSuchElementException, TimeoutException
 import time
 
 class Test:
-    driver = webdriver.Chrome()
+    options = webdriver.ChromeOptions()
+    options.add_argument("--ignore-certificate-errors")
+    options.add_argument("--allow-insecure-localhost")
+    driver = webdriver.Chrome(options=options)
     def __init__(self, domain):
         try:
             self.driver.implicitly_wait(5)
@@ -42,6 +45,23 @@ class Test:
         try:
             self.driver.find_element(By.ID, "submit").click()
             print("[+] Test submission made.")
+            time.sleep(1)
+        except NoSuchElementException as nsee:
+            print("Error:", nsee)
+
+    def test_sso_login(self):
+        time.sleep(2)
+        try:
+            sso_login_button = self.driver.find_element(By.LINK_TEXT, "Google Sign-in")
+            sso_login_button.click()
+            print("[+] SSO button clicked.")
+            time.sleep(1)
+        except NoSuchElementException as nsee:
+            print("Error:", nsee)
+        try:
+            sso_login_button = self.driver.find_element(By.CSS_SELECTOR, ".aZvCDf:nth-child(2) .yAIK0b")
+            sso_login_button.click()
+            print("[+] SSO button clicked.")
             time.sleep(1)
         except NoSuchElementException as nsee:
             print("Error:", nsee)
