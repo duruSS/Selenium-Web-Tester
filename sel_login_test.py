63f4bcadd370151e2ae0b450de77e8eab86afd06
BarisYildirim7 <baris.yildirim@engardens.com>
2025-03-02 22:45:54 +0300

    Comments added for test cases

--- sel_login_test.py
+++ sel_login_test.py
@@ -3,25 +3,19 @@ from test import Test
 if __name__ == "__main__":
     tester = Test("https://127.0.0.1:5000")
     
-    # Regular login Tests
-    tester.test_login("baris@gmail.com", "asdasdasd")
-    
-    tester.test_login("baris@gmail.com", "test123")
-    
-    tester.test_login("baris@gmail.com", "test1234")
+    # Regular login Tests Cases
 
-    tester.test_login("baris@gmail.com", "' 1==1--")
+    tester.test_login("", "")  #Test Case 1: No e-mail & password.
+    
+    tester.test_login("", "12345") #Test Case 2: No e-mail.
+    
+    tester.test_login("duru.solakoglu@ug.bilkent", "") #Test Case 3: No password. Warning: Invalid credentials
+
+    tester.test_login("duru.solakoglu@ug.bilkent", "12345") #Test Case 4: Invalid password. Warning: Invalid credentials
+
+    tester.test_login("duru.solakoglu@ug.bilkent", "12345678") #Test Case 5: Valid mail & password
 
     # This test would not work in a realistic scenario
     # since SSO login procedure cannot be automized.
-    tester.test_sso_login()
 
-    tester.test_login("", "")  #No e-mail & password. Pop-Up: Lütfen bu alanı doldurunuz (points email box)
-    
-    tester.test_login("", "12345") #No e-mail. Pop-Up: Lütfen bu alanı doldurunuz (points email box)
-    
-    tester.test_login("duru.solakoglu@ug.bilkent", "") #No password. Warning: Invalid credentials
-
-    tester.test_login("duru.solakoglu@ug.bilkent", "12345") #Invalid password. Warning: Invalid credentials
-
-    tester.test_login("duru.solakoglu@ug.bilkent", "12345678") # Valid mail & password
+    tester.test_sso_login() #Test Case 6: Testing Google SSO 
